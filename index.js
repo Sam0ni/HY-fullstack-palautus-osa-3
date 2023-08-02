69a085c1e69a97517a75b6225ca6887fb11108e7
Samppa Salo <samppasalo16@gmail.com>
2023-08-02 23:29:29 +0300

    add exercise 3.1

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+
+let persons = [
+    {
+        id: 1,
+        name: "Masayoshi Takanaka",
+        number: "81-22-4466778"
+    },
+    {
+        id: 2,
+        name: "Edward Normal",
+        number: "040-1234321"
+    },
+    {
+        id: 3,
+        name: "Dutch Schaefer",
+        number: "22-22-5051505"
+    }
+
+]
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT)
