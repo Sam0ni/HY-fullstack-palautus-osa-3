aa1c6fce254ca7c8b2efd8c1bb442eeeb449d0df
Samppa Salo <samppasalo16@gmail.com>
2023-08-04 14:44:44 +0300

    add exercises 3.15-3.18

--- index.js
+++ index.js
@@ -32,24 +32,29 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/info", (request, response) => {
     curTime = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${curTime}</p>`)
+    Person.count({}).then(count =>
+        response.send(`<p>Phonebook has info for ${count} people.</p><p>${curTime}</p>`))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(per => per.id === id)
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(result => {
+        if (result) {
+            response.json(result)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
     
-    response.status(204).end()
+})
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post("/api/persons", (request, response) => {
@@ -69,6 +74,26 @@ app.post("/api/persons", (request, response) => {
         response.json(savedPerson)
     })
 })
+
+app.put("/api/persons/:id", (request, response, next) => {
+    const person = Person(request.body)
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
